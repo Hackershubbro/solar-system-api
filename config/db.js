8c90fcc4062748ec25a85d1472f46ee2751d3fe5
Hackershubbro <ramyaseenu2004@gmail.com>
2026-04-01 14:10:40 +0530

    env changes and dockerfile

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 module.exports = mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
+    console.log(process.env.MONGO_URL)
     console.log("Connected to the database")
 }).catch((err) => {
+    console.log(process.env.MONGO_URL)
     console.log("Error connecting to the database", err);
 })
